61ddcc9ca24fbc0ad6129f16f5480b3e3862cdd0
Evgeniy <zhenf@lenta.ru>
2026-04-03 14:20:18 +0300

    -

--- 04_arithmetics_and_pipelining/04_11_sqrt_formula_distributor/04_11_sqrt_formula_distributor.sv
+++ 04_arithmetics_and_pipelining/04_11_sqrt_formula_distributor/04_11_sqrt_formula_distributor.sv
@@ -42,6 +42,107 @@ module sqrt_formula_distributor
     // Hint:
     // Instantiate sufficient number of "formula_1_impl_1_top", "formula_1_impl_2_top",
     // or "formula_2_top" modules to achieve desired performance.
+    localparam CAL_DELAY = (formula == 1 && impl == 1) ? 53 : (formula == 1 && impl == 2) ? 53 : (formula == 2) ? 53 : 0;
+
+    logic [                 31:0] a_array       [0:CAL_DELAY-1];
+    logic [                 31:0] b_array       [0:CAL_DELAY-1];
+    logic [                 31:0] c_array       [0:CAL_DELAY-1];
+    logic [        CAL_DELAY-1:0] arg_vld_array;
+    logic [        CAL_DELAY-1:0] res_vld_array;
+    logic [                 31:0] res_array     [0:CAL_DELAY-1];
+    logic [                 31:0] res_array_temp[0:CAL_DELAY-1];
+    logic [$clog2(CAL_DELAY)-1:0] cur_idx;
+
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+        cur_idx <= '0;
+        end else begin
+        if (arg_vld) begin
+            if (cur_idx < CAL_DELAY -1) cur_idx <= cur_idx + 1;
+            else cur_idx <= 'b0;
+        end
+        end
+    end
+
+    generate
+        genvar i;
+        for (i = 0; i < CAL_DELAY; i = i + 1) begin : gen_block
+        if (formula == 1 && impl == 1) begin
+            formula_1_impl_1_top u_formula_1_impl_1_top (
+            .clk    (clk),
+            .rst    (rst),
+            .arg_vld(arg_vld_array[i]),
+            .a      (a_array[i]),
+            .b      (b_array[i]),
+            .c      (c_array[i]),
+            .res_vld(res_vld_array[i]),
+            .res    (res_array[i])
+            );
+        end else if (formula == 1 && impl == 2) begin
+            formula_1_impl_2_top u_formula_1_impl_2_top (
+            .clk    (clk),
+            .rst    (rst),
+            .arg_vld(arg_vld_array[i]),
+            .a      (a_array[i]),
+            .b      (b_array[i]),
+            .c      (c_array[i]),
+            .res_vld(res_vld_array[i]),
+            .res    (res_array[i])
+            );
+        end else if (formula == 2 && impl == 1) begin
+            formula_2_top u_formula_2_top (
+            .clk    (clk),
+            .rst    (rst),
+            .arg_vld(arg_vld_array[i]),
+            .a      (a_array[i]),
+            .b      (b_array[i]),
+            .c      (c_array[i]),
+            .res_vld(res_vld_array[i]),
+            .res    (res_array[i])
+            );
+        end
+        end
+    endgenerate
+
+
+    generate
+        genvar j;
+        for (j = 0; j < CAL_DELAY; j = j + 1) begin : gen_input
+        always_ff @(posedge clk or posedge rst) begin
+            if (rst) begin
+                a_array[j]       <= 32'b0;
+                b_array[j]       <= 32'b0;
+                c_array[j]       <= 32'b0;
+                arg_vld_array[j] <= 1'b0;
+            end else begin
+            if (arg_vld && cur_idx == j) begin
+                a_array[j]       <= a;
+                b_array[j]       <= b;
+                c_array[j]       <= c;
+                arg_vld_array[j] <= 1'b1;
+            end else begin
+                arg_vld_array[j] <= 1'b0;
+            end
+            end
+        end
+        end
+    endgenerate
+
+    generate
+        genvar k;
+        for (k = 0; k < CAL_DELAY; k = k + 1) begin : gen_output
+        assign res_array_temp[k] = res_array[k] & {32{res_vld_array[k]}};
+        end
+    endgenerate
+
+    assign res_vld = |res_vld_array;
+    always_comb begin
+        res = '0;  // Результат инициализации равен нулю.
+        for (int l = 0; l < CAL_DELAY; l++) begin
+        res |= res_array_temp[l];  // Побитовая операция ИЛИ
+        end
+    end
+
 
 
 endmodule
