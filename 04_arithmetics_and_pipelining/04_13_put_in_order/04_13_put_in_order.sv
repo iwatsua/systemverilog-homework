a1be260e48d53d887f44256e8aa41753b170b2f3
Evgeniy <zhenf@lenta.ru>
2026-04-08 13:08:07 +0300

    -

--- 04_arithmetics_and_pipelining/04_13_put_in_order/04_13_put_in_order.sv
+++ 04_arithmetics_and_pipelining/04_13_put_in_order/04_13_put_in_order.sv
@@ -30,5 +30,68 @@ module put_in_order
     // The idea of the block is kinda similar to the "parallel_to_serial" block
     // from Homework 2, but here block should also preserve the output order.
 
+    localparam ADDR_WIDTH = 16;
+    localparam CNT_WIDTH = $clog2(n_inputs);
+    logic [ n_inputs - 1 : 0][width    - 1 : 0] down_data_pre;
+    logic [ n_inputs - 1 : 0]                   rd_en_array;
+
+    logic [ n_inputs - 1 : 0]                   rd_empty_array;
+    logic [CNT_WIDTH - 1 : 0]                   cnt;
+    logic [CNT_WIDTH - 1 : 0]                   cnt_d;
+
+    assign down_data = down_data_pre[cnt_d];
+
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+        cnt <= '0;
+        end else begin
+        if (|rd_en_array) begin
+            cnt <= cnt + 1;
+        end
+        end
+    end
+
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            cnt_d <= '0;
+        end else begin
+            cnt_d <= cnt;
+        end
+    end
+
+    // fifo need one cycle to read out data, so we need to delay the down_data_pre
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            down_vld <= 1'b0;
+        end else begin
+            down_vld <= |rd_en_array;
+        end
+    end
+
+
+    always_comb begin
+        for (int k = 0; k < n_inputs; k++) begin
+            rd_en_array[k] = (cnt == CNT_WIDTH'(k) && (~rd_empty_array[k]));
+        end
+    end
+
+    generate
+        genvar i;
+        for (i = 0; i < n_inputs; i = i + 1) begin : gen_blk_fifo
+        fifo #(
+            .DATA_LEN  (width),
+            .ADDR_WIDTH(ADDR_WIDTH)
+        ) u_fifo (
+            .clk     (clk),
+            .rst_n   (~rst),
+            .data_in (up_data[i]),
+            .data_out(down_data_pre[i]),
+            .wr_en   (up_vlds[i]),
+            .rd_en   (rd_en_array[i]),
+            .wr_full (),
+            .rd_empty(rd_empty_array[i])
+        );
+        end
+    endgenerate
 
 endmodule
